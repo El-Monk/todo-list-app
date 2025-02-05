ae3d6dba34031c1ec6a0e5dcdc19536473df4663
MSI <djaffarov.roman@yandex.ru>
2025-02-05 22:22:07 +0300

    Initial commit

---
+++ src/main/java/entity/Task.java
@@ -0,0 +1,20 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+    private boolean completed;
+}
